1d7c22e9609f7c8cc40996e6f6eeee12dfceb12e
iliyangenkov <igenkov5@gmail.com>
2024-11-08 15:51:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 import numpy as np
 #from keras.models import load_model
 
+if st.button("CLICK ME")
+  st.balloons()
+
+
 # # Import the class labels from labels.txt and assign to a list
 # classes = [' '.join(x.split(' ')[1:]).replace('\n','') for x in open('labels.txt', 'r').readlines()]
 # # Load the Model
